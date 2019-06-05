6980e9ba3fb26d171440e8ea51bdfbb5b8d4f3b5
Christian M. Jensen <cmj@cjpg.dk>
2019-06-05 10:11:58 +0200

    Initial commit

---
+++ src/SALearnCommand.php
@@ -0,0 +1,88 @@
+<?php
+namespace CJPGDK\SALearn;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+/**
+ * Description of SALearnCommand
+ *
+ * @author Christian M. Jensen
+ */
+class SALearnCommand extends Command
+{
+    private $binWhich = '/usr/bin/which'; 
+    
+    public function __construct($name = null) {
+        parent::__construct('sa-learn');
+    }
+
+    protected function configure()
+    {
+        $this->addOption('ham', '', InputOption::VALUE_NONE, 'Learn messages as ham (non-spam)', null);
+        $this->addOption('spam', '', InputOption::VALUE_NONE, 'Learn messages as spam', null);
+        $this->addOption('forget', '', InputOption::VALUE_NONE, 'Forget a message', null);
+        $this->addOption('use-ignores', '', InputOption::VALUE_NONE, 'Use bayes_ignore_from and bayes_ignore_to', null);
+        $this->addOption('sync', '', InputOption::VALUE_NONE, 'Synchronize the database and the journal if needed', null);
+        $this->addOption('force-expire', '', InputOption::VALUE_NONE, 'Force a database sync and expiry run', null);
+        $this->addOption('dbpath', '', InputOption::VALUE_REQUIRED, 'Allows commandline override (in bayes_path form) for where to read the Bayes DB from', null);
+        $this->addOption('dump', '', InputOption::VALUE_REQUIRED, '[all|data|magic]  Display the contents of the Bayes database, Takes optional argument for what to display', 'all');
+        $this->addOption('regexp', '', InputOption::VALUE_REQUIRED, 'For dump only, specifies which tokens to dump based on a regular expression.', null);
+        $this->addOption('folders', 'f', InputOption::VALUE_REQUIRED, 'Read list of files/directories from file', null);
+        $this->addOption('mbox', '', InputOption::VALUE_NONE, 'Input sources are in mbox format', null);
+        $this->addOption('mbx', '', InputOption::VALUE_NONE, 'Input sources are in mbx format', null);
+        $this->addOption('max-size', '', InputOption::VALUE_REQUIRED, 'Skip messages larger than max-size in bytes, 0 implies no limit (default: 0)', '0');
+        $this->addOption('no-sync', '', InputOption::VALUE_NONE, 'Skip synchronizing the database and journal after learning', null);
+        $this->addOption('local', 'L', InputOption::VALUE_NONE, 'Operate locally, no network accesses', null);
+        $this->addOption('import', '', InputOption::VALUE_NONE, 'Migrate data from older version/non DB_File based databases', null);
+        $this->addOption('clear', '', InputOption::VALUE_NONE, 'Wipe out existing database', null);
+        $this->addOption('backup', '', InputOption::VALUE_REQUIRED, 'Backup, to file, existing database', null);
+        $this->addOption('restore', '', InputOption::VALUE_REQUIRED, 'Restore a database from filename', null);
+        $this->addOption('username', 'u', InputOption::VALUE_REQUIRED, 'Override username taken from the runtime environment, used with SQL', null);
+        $this->addOption('configpath', 'C', InputOption::VALUE_REQUIRED, 'Path to standard configuration dir', null);
+        $this->addOption('config-file', '', InputOption::VALUE_REQUIRED, 'Path to standard configuration dir', null);
+        $this->addOption('prefspath', 'p', InputOption::VALUE_REQUIRED, 'Path to standard configuration dir', null);
+        $this->addOption('prefs-file', '', InputOption::VALUE_REQUIRED, 'Set user preferences file', null);
+        $this->addOption('siteconfigpath', '', InputOption::VALUE_REQUIRED, 'Path for site configs (default: /usr/etc/spamassassin)', '/usr/etc/spamassassin');
+        $this->addOption('cf', '', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Additional line of configuration', null);
+        $this->addOption('debug', 'D', InputOption::VALUE_REQUIRED, '[area=n,...] Print debugging messages', null);
+        $this->addOption('version', 'V', InputOption::VALUE_NONE, 'Print version', null);
+        
+        $this->addArgument('file', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'Files and folder to read');
+        
+        // build in thing.
+        $this->addOption('showdots', '', InputOption::VALUE_NONE, 'Show progress using dots', null);
+        $this->addOption('progress', '', InputOption::VALUE_NONE, 'Show progress using progress bar', null);
+        
+        // locate which
+        foreach (array(
+            '/usr/bin/which',
+            '/bin/which'
+        ) as $binWhich) {
+            if (file_exists($binWhich)) {
+                $this->binWhich = $binWhich;
+                break;
+            }
+        }
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) 
+    {
+        $files = $input->getArgument('file');
+        
+        $output     = null;
+        $return_var = null;
+        exec(escapeshellcmd($this->binWhich.' sa-learn'), $output, $return_var);
+        print_r($output);
+        print_r($return_var);
+    }
+    
+    
+    private function locateBin($name) {
+        
+    }
+
+}
